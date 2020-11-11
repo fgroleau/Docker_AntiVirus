b16cfeede3ac69456cd0803982d539f63c2b870f
Felix Groleau <fgroleau@innovasys.ca>
2020-11-11 17:37:19 -0500

    Did some major change

--- dockerfile
+++ dockerfile
@@ -1,7 +1,10 @@
-FROM python:3.9.0-buster
+FROM ubuntu
 RUN apt update && apt upgrade -y
+RUN DEBIAN_FRONTEND="noninteractive" apt -y install tzdata 
 RUN mkdir /Scan /ToScan
-RUN apt install -y clamav-daemon clamav-freshclam clamav-unofficial-sigs 
+RUN apt install -y clamav-daemon clamav-freshclam clamav-unofficial-sigs python3 python3-pip
+RUN mkdir /var/run/clamav && chown clamav:clamav /var/run/clamav && chmod 750 /var/run/clamav
+RUN service clamav-daemon start && service clamav-freshclam restart
 RUN freshclam && clamscan
 COPY requirement.txt .
 RUN pip3 install -r requirement.txt
--- main.py
+++ main.py
@@ -7,36 +7,44 @@ import clamd
 def UpdateClamAV():
     while True :
         try:
+            #sleep(60)
             system("freshclam")
             sleep(86400)  #24hour
         except : pass
+try :
+    
+    system("service clamav-daemon start") #IMPORTANT
+    VirusKiller = clamd.ClamdUnixSocket()
+    oldlist = list()
+    newlist = list()
+    PathScan = getcwd()+"/Scan/"
+    PathToScan = getcwd()+"/ToScan/"
+    ThreadUpdate = Thread(target=UpdateClamAV)
+    ThreadUpdate.start()
+    while VirusKiller.ping() != "PONG" : pass
+    print("init pass") 
+    
+except Exception as e: print("Erreur survenu : " + str(e))
 
-VirusKiller = clamd.ClamdUnixSocket()
-oldlist = list()
-newlist = list()
-PathScan = getcwd()+"/Scan/"
-PathToScan = getcwd()+"/ToScan/"
-ThreadUpdate = Thread(target=UpdateClamAV)
-ThreadUpdate.start()
-while VirusKiller.ping() != "PONG":pass
+while True:
+    
+    try:
+        newlist = listdir(PathToScan)
 
-while True: 
-    newlist = listdir("ToScan")
+        if newlist != oldlist : 
+            for i in newlist:
+                file = PathToScan+i
+                scanReport = VirusKiller.scan(file)
+                if scanReport[file][0] == 'OK': 
+                    move(file,PathScan)
+                else : 
+                    remove(file)
+                    print(scanReport)
 
-    if newlist != oldlist : 
-        for i in newlist:
-            file = PathToScan+i
-            scanReport = VirusKiller.scan(file)
-            if scanReport[file][0] == 'OK': 
-                move(file,PathScan)
-            else : 
-                remove(file)
-                print(scanReport)
-
-        oldlist = listdir("ToScan")
-    else :
-        oldlist = newlist
-
+            oldlist = listdir(PathToScan)
+        else :
+            oldlist = newlist
+    except Exception as e:pass#print(str(e))
 
 
 
